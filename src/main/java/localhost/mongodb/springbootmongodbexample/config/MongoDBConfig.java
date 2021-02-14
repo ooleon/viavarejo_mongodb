2251e6b1b090565071cd5a16e84ed979f592afaf
ooleon <ooleon@hotmail.com>
2021-02-14 20:41:28 -0300

    update CRUD

--- src/main/java/localhost/mongodb/springbootmongodbexample/config/MongoDBConfig.java
+++ src/main/java/localhost/mongodb/springbootmongodbexample/config/MongoDBConfig.java
@@ -9,6 +9,7 @@ import localhost.mongodb.springbootmongodbexample.repository.UserRepository;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,29 +18,24 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories(basePackageClasses = UserRepository.class)
 @Configuration
 public class MongoDBConfig {
-
+	@Autowired
+	ClienteRepository clienteRepository;
+	
+	@Autowired
+	ApoliceRepository apoliceRepository;
+	
 	@Bean
 	CommandLineRunner commandLineRunner(UserRepository userRepository) {
 		return strings -> {
 			userRepository.save(new Users(1, "Peter", "Development", 3000L));
 			userRepository.save(new Users(2, "Sam", "Operations", 2000L));
 
-		};
-	}
 
-	@Bean
-	CommandLineRunner commandLineRunner(ClienteRepository clienteRepository) {
-		return strings -> {
 			clienteRepository.save(new Clientes(12345678901L, "Joao", "Curitiba", "PR"));
 			clienteRepository.save(new Clientes(12345678902L, "Maria", "Sao Paulo", "SP"));
 			clienteRepository.save(new Clientes(12345678903L, "Ana", "Rio de Janeiro", "RJ"));
 
-		};
-	}
 
-	@Bean
-	CommandLineRunner commandLineRunner(ApoliceRepository apoliceRepository) {
-		return strings -> {
 			apoliceRepository.save(new Apolices(44445678901L, new Date(), new Date(), "ERT345", 123400.1D));
 			apoliceRepository.save(new Apolices(44445678901L, new Date(), new Date(), "SER678", 123400.2D));
 			apoliceRepository.save(new Apolices(44445678901L, new Date(), new Date(), "CVB456", 123400.3D));
