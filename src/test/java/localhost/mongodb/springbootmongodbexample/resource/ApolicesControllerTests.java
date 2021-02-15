28860df33f85c0f0494887bf18cd63da0692b610
ooleon <ooleon@hotmail.com>
2021-02-15 18:10:02 -0300

    update EndPoints Test

--- src/test/java/localhost/mongodb/springbootmongodbexample/resource/ApolicesControllerTests.java
+++ src/test/java/localhost/mongodb/springbootmongodbexample/resource/ApolicesControllerTests.java
@@ -5,7 +5,12 @@ import localhost.mongodb.springbootmongodbexample.document.Clientes;
 import localhost.mongodb.springbootmongodbexample.repository.ApoliceRepository;
 import localhost.mongodb.springbootmongodbexample.service.ApolicesService;
 
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,23 +23,29 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.logging.Logger;
 
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ApolicesControllerTests {
 //	public class ApolicesResource {
     private final Logger logger = Logger.getLogger(this.getClass().getName());
-    
+
+	@LocalServerPort
+	private int port;
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
     @Autowired
     private ApolicesService apolicesService;
     
-
-//    public ApolicesController(ApoliceRepository apoliceRepository) {
-//        this.apoliceRepository = apoliceRepository;
-//    }
-
 //    @GetMapping("/all")
-    public List<Apolices> getAll() {
-        return this.apolicesService.findAll();
+    @Test
+    public void getAll() {
+    	String msg = this.restTemplate.getForObject("http://localhost:" + port + "/",
+				String.class);
+    	
+    	System.out.println(msg);
+//        this.apolicesService.findAll();
     }
-    
+    /*
 //	@RequestMapping(value = { "/salvar" }, method = { RequestMethod.POST })
 	public ResponseEntity<Apolices> salvar(@RequestBody Apolices a) {
 		return new ResponseEntity(this.apolicesService.save(a), HttpStatus.OK);
@@ -45,5 +56,5 @@ public class ApolicesControllerTests {
     	
         return apolicesService.findOne(id);
     }
-    
+    */
 }
